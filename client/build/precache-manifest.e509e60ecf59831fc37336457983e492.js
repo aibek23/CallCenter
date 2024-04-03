b7e4da35735d4127ac5f2b28446dbb33b19829aa
aibek23 <aibekasanakunuul@gmail.com>
2024-04-03 12:23:37 +0600

    first

---
+++ client/build/precache-manifest.e509e60ecf59831fc37336457983e492.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aad50b350a338432b5e59ee6e8b991ca",
+    "url": "./index.html"
+  },
+  {
+    "revision": "cf3845bbad6e0cf09163",
+    "url": "./static/css/2.6684f297.chunk.css"
+  },
+  {
+    "revision": "0b597feb843a65be9516",
+    "url": "./static/css/main.2c0f97ae.chunk.css"
+  },
+  {
+    "revision": "cf3845bbad6e0cf09163",
+    "url": "./static/js/2.9d79db23.chunk.js"
+  },
+  {
+    "revision": "0603ea3195b67b8eb924c0e8a54e9dbd",
+    "url": "./static/js/2.9d79db23.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0b597feb843a65be9516",
+    "url": "./static/js/main.be5e9d16.chunk.js"
+  },
+  {
+    "revision": "2fc385f0f34e1fb1a817",
+    "url": "./static/js/runtime-main.7dcb2316.js"
+  },
+  {
+    "revision": "d2aff3a977f23ff787abae1b3701a5af",
+    "url": "./static/media/Call_center.d2aff3a9.jpg"
+  },
+  {
+    "revision": "cd4ba0554daae44b03f48709aac326af",
+    "url": "./static/media/background-image.cd4ba055.jpg"
+  },
+  {
+    "revision": "e4462ef2266a748bfa6118eeb85891cc",
+    "url": "./static/media/call_management.icon.e4462ef2.png"
+  },
+  {
+    "revision": "39578caf1ffaac87fb6856a322857c8d",
+    "url": "./static/media/kisspng-avatar-user-medicine-surgery.39578caf.jpg"
+  },
+  {
+    "revision": "7d22c62510ee430a89ed397e343db06a",
+    "url": "./static/media/png-clipart-customer-service-call-centre.png.7d22c625.gif"
+  },
+  {
+    "revision": "8a6a45449dc516648789a6db61ba7dc3",
+    "url": "./static/media/skayp-call.8a6a4544.mp3"
+  },
+  {
+    "revision": "1e3e7b810795d43889b728c25780b6b8",
+    "url": "./static/media/toot.1e3e7b81.mp3"
+  }
+]);
